e776dc583edbc1878580ab2e387072a21363bed2
Tanna Infotech <infotechtanna@gmail.com>
2024-05-11 15:08:32 +0530

    update library

--- README.md
+++ README.md
@@ -5,3 +5,73 @@ A Project for get date methods
 ## Feature
 
 - Get date function
+
+&#10240; <!-- line space -->
+&#10240; <!-- line space -->
+
+
+## Example
+
+1. Convert milliseconds to date
+
+    - Example 1:
+ 
+          DateUtility.millisecondToDate(1715365800000, 'yyyy-MM-dd');    // 2024-05-11
+      
+    - Example 2:
+ 
+          DateUtility.millisecondToDate(1715365800000, 'd MMM yyyy');    // 11 May 2024
+          
+2. Convert date to milliseconds
+
+    - Example 1:
+ 
+          DateUtility.dateToMillisecond('2024-05-11');    // 1715365800000
+      
+    - Example 2:
+ 
+          DateUtility.dateToMillisecond('11 May 2024');    // 1715365800000
+
+3. Change date format
+
+    - Example 1:
+ 
+          DateUtility.changeDateFormat('2024-05-11', 11 May 2024);    // 2024-05-11 convert to 11 May 2024
+      
+    - Example 2:
+ 
+          DateUtility.changeDateFormat('11 May 2024', '2024-05-11');    // 11 May 2024 convert to 2024-05-11
+
+4. Count days
+  
+    - Example 1:
+ 
+          DateUtility.daysBetween('2024-05-10', '2024-05-11');    // 1 days
+      
+    - Example 2:
+ 
+          DateUtility.daysBetweenWithMilliSecond(1715279400000, 1715365800000);    // 1 days
+
+5. Time ago
+   
+    - Example 1:
+ 
+          DateUtility.timeAgoSinceDate('2024-05-10', true);    // 1 day ago
+      
+    - Example 2:
+ 
+          DateUtility.timeAgoSinceMilliseconds(1715279400000, true);    // 1 days ago
+
+      | numericDates = true | numericDates = false |
+      |-------------------- | -------------------- |
+      | 1 minute ago        | A minute ago         |
+      | 1 hour ago          | An hour ago          |  
+      | 1 day ago           | Yesterday            |
+      | 1 week ago          | Last week            |
+      | 1 month ago         | Last month           |
+      | 1 year ago          | Last year            |
+
+
+
+
+   
--- lib/date_utility.dart
+++ lib/date_utility.dart
@@ -1,7 +1,162 @@
 library date_utility;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:intl/intl.dart';
+
+class DateUtility {
+  String millisecondToDate(int timestamp,
+      {String formatPattern = 'yyyy-MM-dd'}) {
+    try {
+      DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+      DateFormat dateFormatee = DateFormat(formatPattern);
+      String dateFormat = dateFormatee.format(dateTime);
+      return dateFormat;
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
+
+  /// [dateTomillisecond] this function convert date into millisecond
+  dynamic dateToMillisecond(String timestamp) {
+    try {
+      if (timestamp == '') {
+        return DateTime.now().millisecondsSinceEpoch;
+      }
+      DateTime dateTime = DateTime.parse(timestamp);
+
+      int dateFormat = dateTime.millisecondsSinceEpoch;
+      return dateFormat;
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
+
+  String changeDateFormat(
+      String currentFormatPattern, String aspectedFormatPattern) {
+    try {
+      DateTime date = DateTime.parse(currentFormatPattern);
+      DateFormat dateFormatee = DateFormat(aspectedFormatPattern);
+      String dateFormat = dateFormatee.format(date);
+      return dateFormat;
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
+
+  // String changeDateFormatWithMilliSecond(
+  //     int currentMilliSecond, String aspectedFormatPattern) {
+  //   try {
+  //     DateTime date = DateTime.fromMillisecondsSinceEpoch(currentMilliSecond);
+  //     DateFormat dateFormatee = DateFormat(aspectedFormatPattern);
+  //     String dateFormat = dateFormatee.format(date);
+  //     return dateFormat;
+  //   } catch (e) {
+  //     return 'InValid date format';
+  //   }
+  // }
+
+  dynamic daysBetween(String fromDate, String toDate) {
+    try {
+      DateTime from = DateTime.parse(fromDate);
+      DateTime to = DateTime.parse(toDate);
+      from = DateTime(from.year, from.month, from.day);
+      to = DateTime(to.year, to.month, to.day);
+      return (to.difference(from).inHours / 24).round();
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
+
+  dynamic daysBetweenWithMilliSecond(
+      int fromDateMillisecond, int toDateMillisecond) {
+    try {
+      DateTime from = DateTime.fromMillisecondsSinceEpoch(fromDateMillisecond);
+      DateTime to = DateTime.fromMillisecondsSinceEpoch(toDateMillisecond);
+      from = DateTime(from.year, from.month, from.day);
+      to = DateTime(to.year, to.month, to.day);
+      return (to.difference(from).inHours / 24).round();
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
+
+  String timeAgoSinceDate(String dateString, {bool numericDates = true}) {
+    try {
+      DateTime date = DateTime.parse(dateString);
+      final date2 = DateTime.now();
+      final difference = date2.difference(date);
+
+      if ((difference.inDays / 365).floor() >= 2) {
+        return '${(difference.inDays / 365).floor()} years ago';
+      } else if ((difference.inDays / 365).floor() >= 1) {
+        return (numericDates) ? '1 year ago' : 'Last year';
+      } else if ((difference.inDays / 30).floor() >= 2) {
+        return '${(difference.inDays / 365).floor()} months ago';
+      } else if ((difference.inDays / 30).floor() >= 1) {
+        return (numericDates) ? '1 month ago' : 'Last month';
+      } else if ((difference.inDays / 7).floor() >= 2) {
+        return '${(difference.inDays / 7).floor()} weeks ago';
+      } else if ((difference.inDays / 7).floor() >= 1) {
+        return (numericDates) ? '1 week ago' : 'Last week';
+      } else if (difference.inDays >= 2) {
+        return '${difference.inDays} days ago';
+      } else if (difference.inDays >= 1) {
+        return (numericDates) ? '1 day ago' : 'Yesterday';
+      } else if (difference.inHours >= 2) {
+        return '${difference.inHours} hours ago';
+      } else if (difference.inHours >= 1) {
+        return (numericDates) ? '1 hour ago' : 'An hour ago';
+      } else if (difference.inMinutes >= 2) {
+        return '${difference.inMinutes} minutes ago';
+      } else if (difference.inMinutes >= 1) {
+        return (numericDates) ? '1 minute ago' : 'A minute ago';
+      } else if (difference.inSeconds >= 3) {
+        return '${difference.inSeconds} seconds ago';
+      } else {
+        return 'Just now';
+      }
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
+
+  String timeAgoSinceMilliseconds(int milliseconds,
+      {bool numericDates = true}) {
+    try {
+      DateTime date = DateTime.fromMillisecondsSinceEpoch(milliseconds);
+      final date2 = DateTime.now();
+      final difference = date2.difference(date);
+
+      if ((difference.inDays / 365).floor() >= 2) {
+        return '${(difference.inDays / 365).floor()} years ago';
+      } else if ((difference.inDays / 365).floor() >= 1) {
+        return (numericDates) ? '1 year ago' : 'Last year';
+      } else if ((difference.inDays / 30).floor() >= 2) {
+        return '${(difference.inDays / 30).floor()} months ago';
+      } else if ((difference.inDays / 30).floor() >= 1) {
+        return (numericDates) ? '1 month ago' : 'Last month';
+      } else if ((difference.inDays / 7).floor() >= 2) {
+        return '${(difference.inDays / 7).floor()} weeks ago';
+      } else if ((difference.inDays / 7).floor() >= 1) {
+        return (numericDates) ? '1 week ago' : 'Last week';
+      } else if (difference.inDays >= 2) {
+        return '${difference.inDays} days ago';
+      } else if (difference.inDays >= 1) {
+        return (numericDates) ? '1 day ago' : 'Yesterday';
+      } else if (difference.inHours >= 2) {
+        return '${difference.inHours} hours ago';
+      } else if (difference.inHours >= 1) {
+        return (numericDates) ? '1 hour ago' : 'An hour ago';
+      } else if (difference.inMinutes >= 2) {
+        return '${difference.inMinutes} minutes ago';
+      } else if (difference.inMinutes >= 1) {
+        return (numericDates) ? '1 minute ago' : 'A minute ago';
+      } else if (difference.inSeconds >= 3) {
+        return '${difference.inSeconds} seconds ago';
+      } else {
+        return 'Just now';
+      }
+    } catch (e) {
+      return 'InValid date format';
+    }
+  }
 }
